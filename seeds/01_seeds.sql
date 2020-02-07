1cac9ecce0ad9d4ea3f85197aefb18cc3a08aeb2
pishili <pishili@users.noreply.github.com>
2020-02-07 01:44:07 -0800

    insert values

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO users (name, email) 
+VALUES ('Eva Stanley', 'ada@gmail.com'),
+('Eva adsadaStanley', 'asdasada@gmail.com'),
+('Eva asdadStanley', 'asdadsaada@gmail.com');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces,number_of_bathrooms,number_of_bedrooms,country
+, street,city,provincepost_code,active)
+VALUES (1, 'adsad', 'adSASDada', '2018-09-26', '2018-09-26', '2018-09-26', 122SA3, 5, 1,2, 'IRAN', 'sasa,', 'TEHRAN', 'AAD', FALSE),
+(2, 'aADSADSAdsad', 'adaADda', '2018-09-26', '2018-09-26', '2018-09-26', 122S3, 55, 1,2, 'Canada', 'sasa,', 'Sudbuary', 'v6z', FALSE),
+(3, 'adsaSDASDASDd', 'AAadada', '2018-09-26', '2018-09-26', '2018-09-26', 122DS3, 6, 1,2, 'us', 'sasa,', 'LA', 'v6z', TRUE);
+
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id) 
+VALUES ('2018-09-11', '2018-09-26',1, 1),
+('2019-01-04', '2019-02-01',2, 2),
+('2021-10-01', '2021-10-14',3, 3);
+
+
+INSERT INTO property_reviews (guest_id, property_id,reservation_id, rating, message)
+VALUES (2, 2,2, 10, 'SDSA'),
+(3, 3, 3, 10, 'DASDSA'),
+(4,4,4, 15, 'DASDSADASD');
